9590e926f1d2140dbc611601edda609d608e6eb0
goiizin <hmp77rvw5k@privaterelay.appleid.com>
2025-12-29 21:50:51 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMode() {
+  document.body.classList.toggle("dark");
+}
+
+function showService() {
+  document.getElementById("service-text").innerText =
+    "기내에서 고객이 좌석이나 서비스에 불편을 느낄 경우, 승무원은 침착하고 친절한 태도로 응대하여 고객 만족을 높입니다.";
+}
